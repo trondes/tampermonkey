c607328703740c071c515f5b10a53f612799236f
trondes <44512277+trondes@users.noreply.github.com>
2021-05-11 12:10:20 +0200

    Update SP-Favicon.js

--- SP-Favicon.js
+++ SP-Favicon.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Add favicon
+// @name         SP-Add favicon
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Change favicon. local host
