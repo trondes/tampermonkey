f565e439bc354582d960c8c502dacce61bb3526f
trondes <44512277+trondes@users.noreply.github.com>
2021-05-11 13:16:39 +0200

    Update VFS-keepalive.js

--- VFS-keepalive.js
+++ VFS-keepalive.js
@@ -14,5 +14,5 @@
         if ($('.logout_countdown #dialogContinueButton').length) {
             $('.logout_countdown #dialogContinueButton').trigger('click');
         }
-    }, 10000);
+    }, 3000);
 })();
